94d865a8635449681beafff040c96106b0247119
Alcrro <alex.roventa94@gmail.com>
2022-06-22 05:57:04 +0300

    main

--- index/controllers/index.js
+++ index/controllers/index.js
@@ -1,43 +1,43 @@
 const Product = require('../models/product');
 // const PDFDocument = require('pdfkit')
 
-const ITEMS_PER_PAGE = 2;
-const filter_item_per_page = 10
 
-exports.getIndex = (req, res, next) => {
+exports.getIndex = async (req, res, next) => {
 
 	// + - convert to number
 	const page = +req.query.page || 1;
+	const perPage = 10;
 	let totalItems;
-	Product.find()
-	.countDocuments()
-	.then(numProducts =>{
-		totalItems = numProducts;
-		return Product.find()
-		.skip((page - 1) * ITEMS_PER_PAGE)
-		.limit(ITEMS_PER_PAGE)
-	})
-	.then(products => {
-			// console.log(products);
-      res.render('index/index', {
-        prods: products,
-        pageTitle: 'Index',
-        path: '/',
-				currentPage: page,
-				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-				hasPreviousPage: page > 1,
-				nextPage: page + 1,
-				previousPage: page - 1,
-				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
-				isAuthenticated: req.session.isLoggedIn
-      });
-    })
-    .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
-    });
-};
+	try{
+		const totalItems = await Product.find()
+		.countDocuments()
+		
+		const product = await Product.find()
+		.skip((page - 1) * perPage)
+		.limit(perPage);
+
+				res.render('index/index', {
+					prods: product,
+					pageTitle: 'Index',
+					path: '/',
+					currentPage: page,
+					message: 'Fetched posts successfully',
+					totalItems: totalItems,
+					hasNextPage: perPage * page < totalItems,
+					hasPreviousPage: page > 1,
+					nextPage: page + 1,
+					previousPage: page - 1,
+					lastPage: Math.ceil(totalItems / perPage),
+					isAuthenticated: req.session.isLoggedIn
+				});
+		}
+    catch(err) {
+			if (!err.statusCode){
+				err.statusCode =500
+			}
+		next(err);
+	}
+}
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
@@ -54,27 +54,12 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getSortAsc = (req, res, next) => {
-	const page = +req.query.page || 1;
-	let totalItems;
-	Product.find()
-	.countDocuments()
-	.then(numProducts =>{
-		totalItems = numProducts;
-		return Product.find()
-		.skip((page - 1) * ITEMS_PER_PAGE)
-		.limit(ITEMS_PER_PAGE);
-	})
+	Product.find().sort({price: 1})
     .then(products => {
 			// console.log(products);
       res.render('index/index', {
         prods: products,
         pageTitle: 'Index',
-				currentPage: page,
-				nextPage: page + 1,
-				previousPage: page - 1,
-				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
-				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-				hasPreviousPage: page > 1,
         path: '/',
 				isAuthenticated: req.session.isLoggedIn
       });
@@ -84,31 +69,12 @@ exports.getSortAsc = (req, res, next) => {
     });
 };
 exports.getSortDesc = (req, res, next) => {
-	const page = +req.query.page || 1;
-	let totalItems;
-	Product.find()
-	.countDocuments()
-	.then(numProducts =>{
-		totalItems = numProducts;
-		return Product.find()
-		.skip((page - 1) * ITEMS_PER_PAGE)
-		.sort({price: -1})
-		.limit(ITEMS_PER_PAGE)
-	})
-  // Product.find().sort({price: -1})
-	// .limit(ITEMS_PER_PAGE)
+  Product.find().sort({price: -1})
     .then(products => {
 			// console.log(products);
       res.render('index/index', {
         prods: products,
         pageTitle: 'Index',
-				currentPage: page,
-				price: 1,
-				nextPage: page + 1,
-				previousPage: page - 1,
-				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
-				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-				hasPreviousPage: page > 1,
         path: '/',
 				isAuthenticated: req.session.isLoggedIn
       });
@@ -119,29 +85,12 @@ exports.getSortDesc = (req, res, next) => {
 };
 
 exports.getLimitOne = (req, res, next) => {
-	const page = +req.query.page || 1;
-	let totalItems;
-	Product.find()
-	.countDocuments()
-	.then(numProducts =>{
-		totalItems = numProducts;
-		return Product.find()
-		.skip((page - 1) * ITEMS_PER_PAGE)
-		.sort({price: -1})
-		.limit(ITEMS_PER_PAGE)
-	})
   Product.find().limit(1)
     .then(products => {
 			// console.log(products);
       res.render('index/index', {
         prods: products,
         pageTitle: 'Index',
-				currentPage: page,
-				nextPage: page + 1,
-				previousPage: page - 1,
-				lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
-				hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-				hasPreviousPage: page > 1,
         path: '/',
 				isAuthenticated: req.session.isLoggedIn
       });
--- index/routes/indexRoutes.js
+++ index/routes/indexRoutes.js
@@ -11,15 +11,13 @@ const indexController = require('../controllers/index')
 
 
 router.get('/', indexController.getIndex);
-router.get('/sort-priceasc', indexController.getSortAsc);
-router.get('/sort-pricedesc', indexController.getSortDesc);
-router.get('/sort-priceasc/limit/1', indexController.getLimitOne);
+router.get('/sort-priceasc', indexController.getIndex);
+router.get('/sort-pricedesc', indexController.getIndex);
 router.get('/limit/2', indexController.getLimitTwo);
 router.get('/limit/3', indexController.getLimitTree);
 router.get('/limit/4', indexController.getLimitFour);
 router.get('/products/:productId', indexController.getProduct);
-// router.get('/:page', indexController.getIndex);
-
+// router.get('/:p=:p', indexController.getIndex);
 
 
 
